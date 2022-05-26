d94be9769e3c9d124cb61bb5b1df7d31a133498b
Diego Minelli <diego.minelli@hotmail.com>
2022-05-26 17:26:10 -0300

    testando atributos de acessibilidade

--- src/app/shared/components/like-widget/photo-frame/photo-frame/photo-frame.component.spec.ts
+++ src/app/shared/components/like-widget/photo-frame/photo-frame/photo-frame.component.spec.ts
@@ -51,12 +51,26 @@ describe('PhotoFrameComponent.name', () => {
     expect(times).toBe(2);
   }));
 
-  it(`#Should display number of likes when (@Input likes' is incremented`, () => {
+  it(`Should display number of likes when (@Input likes' is incremented`, () => {
     fixture.detectChanges();
     component.likes++;
     fixture.detectChanges();
     const element: HTMLElement =
       fixture.nativeElement.querySelector('.like-counter');
     expect(element.textContent.trim()).toBe('1');
+  });
+
+  it(`Should update aria-label when (@Input likes) is incremented`, () => {
+    fixture.detectChanges();
+    component.likes++;
+    fixture.detectChanges();
+    const element: HTMLElement = fixture.nativeElement.querySelector('span');
+    expect(element.getAttribute('aria-label')).toBe('1: people liked');
+  });
+
+  it(`Should have aria-label with 0 (@Input likes)`, () => {
+    fixture.detectChanges();
+    const element: HTMLElement = fixture.nativeElement.querySelector('span');
+    expect(element.getAttribute('aria-label')).toBe('0: people liked');
   });
 });
