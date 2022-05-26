03b8155b45380dd545fa60dc6d0594710326a425
Diego Minelli <diego.minelli@hotmail.com>
2022-05-26 16:19:09 -0300

    desenvolvendo o primeiro teste

--- src/app/shared/components/like-widget/photo-frame/photo-frame/photo-frame.component.spec.ts
+++ src/app/shared/components/like-widget/photo-frame/photo-frame/photo-frame.component.spec.ts
@@ -1,25 +1,22 @@
+import { PhotoFrameModule } from './photo-frame.module';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PhotoFrameComponent } from './photo-frame.component';
 
-describe('PhotoFrameComponent', () => {
+describe('PhotoFrameComponent.name', () => {
   let component: PhotoFrameComponent;
-  let fixture: ComponentFixture<PhotoFrameComponent>;
+  let fixture: ComponentFixture<PhotoFrameComponent> = null;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PhotoFrameComponent ]
-    })
-    .compileComponents();
-  });
+      imports: [PhotoFrameModule],
+    }).compileComponents();
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(PhotoFrameComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create component', () => {
     expect(component).toBeTruthy();
   });
 });
