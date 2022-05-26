ad9974dd5dc3ea96bfa494db9da776d29b859d91
Diego Minelli <diego.minelli@hotmail.com>
2022-05-26 15:15:28 -0300

    desenvolvendo o componente photo frame

--- src/app/shared/components/like-widget/photo-frame/photo-frame/photo-frame.component.ts
+++ src/app/shared/components/like-widget/photo-frame/photo-frame/photo-frame.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-photo-frame',
   templateUrl: './photo-frame.component.html',
-  styleUrls: ['./photo-frame.component.scss']
+  styleUrls: ['./photo-frame.component.scss'],
 })
 export class PhotoFrameComponent implements OnInit {
+  @Output() public liked: EventEmitter<void> = new EventEmitter();
+  @Input() public description = '';
+  @Input() public src = '';
+  @Input() public likes = 0;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  public like(): void {
+    this.liked.emit();
   }
-
 }
