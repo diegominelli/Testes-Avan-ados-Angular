a17d378dfa7e4748cce843270729c04a80b40618
Diego Minelli <diego.minelli@hotmail.com>
2022-05-30 12:15:45 -0300

    criacao do componente photoboard

--- src/app/shared/components/like-widget/like-widget.module.ts
+++ src/app/shared/components/like-widget/like-widget.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { UniqueIdService } from '../../services/unique-id/unique-id.service';
 import { LikeWidgetComponent } from './like-widget.component';
+import { PhotoBoardComponent } from './photo-board/photo-board/photo-board.component';
 
 @NgModule({
-  declarations: [LikeWidgetComponent],
+  declarations: [LikeWidgetComponent, PhotoBoardComponent],
   imports: [CommonModule, FontAwesomeModule, ActionDirectiveModule],
   exports: [LikeWidgetComponent],
   providers: [UniqueIdService],
---
+++ src/app/shared/components/like-widget/photo-board/photo-board/photo-board.module.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { PhotoFrameModule } from './../../photo-frame/photo-frame/photo-frame.module';
+import { PhotoBoardComponent } from './photo-board.component';
+
+@NgModule({
+  declarations: [PhotoBoardComponent],
+  imports: [CommonModule, PhotoFrameModule],
+  exports: [PhotoBoardComponent],
+})
+export class PhotoBoardModule {}
