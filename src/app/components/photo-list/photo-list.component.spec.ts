3a08bd5648cb1141d63f8d8517a7f9c83e0bcfda
Diego Minelli <diego.minelli@hotmail.com>
2022-06-01 14:26:56 -0300

    testando componente com escopo de pagina

--- src/app/components/photo-list/photo-list.component.spec.ts
+++ src/app/components/photo-list/photo-list.component.spec.ts
@@ -1,25 +1,23 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PhotoListComponent } from './photo-list.component';
+import { PhotoListModule } from './photo-list.module';
 
-describe('PhotoListComponent', () => {
+describe(PhotoListComponent.name, () => {
   let component: PhotoListComponent;
   let fixture: ComponentFixture<PhotoListComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PhotoListComponent ]
-    })
-    .compileComponents();
-  });
+      imports: [PhotoListModule, HttpClientModule],
+    }).compileComponents();
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(PhotoListComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create component', () => {
     expect(component).toBeTruthy();
   });
 });
