5a6f726afbe73b6237189bf36c6e2389abfe799f
Diego Minelli <diego.minelli@hotmail.com>
2022-06-01 15:06:28 -0300

    finalizando os testes usando o spyOn

--- src/app/components/photo-list/photo-list.component.spec.ts
+++ src/app/components/photo-list/photo-list.component.spec.ts
@@ -4,6 +4,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PhotoListComponent } from './photo-list.component';
 import { PhotoListModule } from './photo-list.module';
+import { buildPhotoList } from 'src/app/shared/components/like-widget/photo-board/test/build-photos-list';
+import { of } from 'rxjs';
 
 describe(PhotoListComponent.name, () => {
   let component: PhotoListComponent;
@@ -25,6 +27,22 @@ describe(PhotoListComponent.name, () => {
   });
 
   it(`(D) Should display board when data arrives`, () => {
+    const photos = buildPhotoList();
+    spyOn(service, 'getPhotos').and.returnValue(of(photos));
     fixture.detectChanges();
+    const board = fixture.nativeElement.querySelector('app-photo-board');
+    const loader = fixture.nativeElement.querySelector('.loader');
+    expect(board).withContext('Should not display board').not.toBeNull();
+    expect(loader).withContext('Should display loader').toBeNull();
+  });
+
+  it(`(D) Should display loader whille waiting for data`, () => {
+    const photos = buildPhotoList();
+    spyOn(service, 'getPhotos').and.returnValue(null);
+    fixture.detectChanges();
+    const board = fixture.nativeElement.querySelector('app-photo-board');
+    const loader = fixture.nativeElement.querySelector('.loader');
+    expect(board).withContext('Should not display board').toBeNull();
+    expect(loader).withContext('Should display loader').not.toBeNull();
   });
 });
